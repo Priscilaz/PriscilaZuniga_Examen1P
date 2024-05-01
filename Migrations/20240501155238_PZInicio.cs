4b2505c7a1b1204d5a45a4d307234c7ce3eb55f3
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-05-01 10:56:34 -0500

    Se han implementado las migraciones a la base de datos

---
+++ Migrations/20240501155238_PZInicio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriscilaZuniga_Examen1P.Migrations
+{
+    /// <inheritdoc />
+    public partial class PZInicio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PZClass",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    PZ_Peso = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PZ_Apodo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PZ_Activo = table.Column<bool>(type: "bit", nullable: false),
+                    PZ_FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PZClass", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PZClass");
+        }
+    }
+}
