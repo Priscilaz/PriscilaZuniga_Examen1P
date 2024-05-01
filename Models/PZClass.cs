1bedab0ff7a77282b36930e99217538ba33197ce
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-05-01 10:46:23 -0500

    Agrego el atributo ID a la clase

--- Models/PZClass.cs
+++ Models/PZClass.cs
@@ -5,6 +5,7 @@ namespace PriscilaZuniga_Examen1P.Models
     public class PZClass
     {
         //Creacion de la Clase
+        public int ID { get; set; }
         public int Edad { get; set; }
         public decimal PZ_Peso { get; set; }
         public string? PZ_Apodo { get; set; }
