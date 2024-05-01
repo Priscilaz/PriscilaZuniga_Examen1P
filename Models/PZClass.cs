36a5cc8951d213ca97f17b4c4c393f0229e0d3c0
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-05-01 10:38:06 -0500

    Cambios en  la clase PZ, donde se definen sus atributos

--- Models/PZClass.cs
+++ Models/PZClass.cs
@@ -1,8 +1,14 @@
-﻿namespace PriscilaZuniga_Examen1P.Models
+﻿using System;
+
+namespace PriscilaZuniga_Examen1P.Models
 {
     public class PZClass
     {
-    //Creacion de la Clase
-
+        //Creacion de la Clase
+        public int Edad { get; set; }
+        public decimal PZ_Peso { get; set; }
+        public string? PZ_Apodo { get; set; }
+        public Boolean PZ_Activo { get; set; }
+        public DateTime PZ_FechaNacimiento { get; set; }
     }
 }
